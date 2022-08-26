f80355b69cda99ddc008c0bc20e7b5d0820b29cd
Lyubov <zenkova.lyuba@gmail.com>
2022-08-27 01:29:55 +0300

    hw1

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--    <button v-on:click="counter++">{{ counter }}</button>-->
+<!--    <button @click="counter++">{{ counter }}</button>-->
+<!--    <button @click="increase(2, $event)">{{ counter }}</button>-->
+<!--    <button @click="reset">Reset</button>-->
+<!--    <input-->
+<!--      type="text"-->
+<!--      placeholder="text"-->
+<!--      @blur="blur"-->
+<!--      @focus="focus"-->
+<!--      @change="change"-->
+<!--      @input="input"-->
+<!--      v-model.lazy="txt"-->
+<!--    >-->
+<!--    <p>{{ txt }}</p>-->
+    <CalcFunc/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import CalcFunc from '@/components/CalcFunc.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    CalcFunc
+  },
+  data: () => ({
+    counter: 0,
+    txt: ''
+  }),
+  methods: {
+    increase (step, event) {
+      console.log(event)
+      this.counter += step
+    },
+    reset () {
+      this.counter = 0
+    },
+    blur () {
+      console.log('blur')
+    },
+    focus () {
+      console.log('focus')
+    },
+    change () {
+      console.log('change')
+    },
+    input () {
+      console.log('input')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CalcFunc.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div class="main">
+      <input type="number" v-model.number="op1">
+      <input type="number" v-model.number="op2">
+      = {{ result }}
+    </div>
+    <div class="keyboard">
+<!--      <button @click="result = +op1 + +op2">+</button>-->
+<!--      <button @click="result = op1 + op2">+</button>-->
+      <button @click="sum">+</button>
+      <button @click="sub">-</button>
+      <button @click="div">/</button>
+      <button @click="mult">*</button>
+      <button @click="pow">^</button>
+      <button @click="trunc">trunc</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CalcFunc',
+  data: () => ({
+    op1: 0,
+    op2: 0,
+    result: 0
+  }),
+  methods: {
+    sum () {
+      this.result = this.op1 + this.op2
+    },
+    div () {
+      this.result = this.op1 / this.op2
+    },
+    sub () {
+      this.result = this.op1 - this.op2
+    },
+    mult () {
+      this.result = this.op1 * this.op2
+    },
+    pow () {
+      this.result = Math.pow(this.op1, this.op2)
+    },
+    trunc () {
+      this.result = Math.trunc(this.op1 / this.op2)
+    }
+  }
+}
+</script>
